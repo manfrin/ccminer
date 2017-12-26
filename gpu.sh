9bf5d56223942cb974b3f34682128a181054f792
Mike Manfrin <manfrin@gmail.com>
2017-12-26 15:20:33 -0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,6 +3,7 @@ echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
 if ! dpkg-query -W cuda-9-1; then
   # The 16.04 installer works with 16.10.
+  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
   curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
   dpkg -i ./cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
   apt-get update
